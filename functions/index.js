eb27e5d9a0cb749a3b08c187f41d881ffa4d9b5a
hanehikako1992 <hanehikako1992@outlook.com>
2022-01-29 15:10:17 +0900

    init

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+const serverless = require('serverless-http');
 
 
-exports.handler = async event => {
-  return {
-    statusCode: 200,
-    body: 'hello world'
-  }
-};
+// app.use(express.static(path.join(__dirname, "")))
+app.use(express.static(path.join(__dirname, 'build')));
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port =  5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/user"));
+// get driver connection
+const dbo = require("./db/conn");
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+
+  });
+  console.log(`Server is running on port: ${port}`);
+});
+
+
+module.exports.handler = serverless(app)
