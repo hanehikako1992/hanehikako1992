f9a274fedb95ee46c09e464765aed84e46cd1972
hanehikako1992 <hanehikako1992@outlook.com>
2022-01-30 20:18:24 +0900

    serverless

--- express/server.js
+++ express/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser')
+const serverless = require('serverless-http')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 const router = express.Router();
