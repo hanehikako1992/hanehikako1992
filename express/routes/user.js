1d51408269cc7caac8d606aa75515bd6273518f3
hanehikako1992 <hanehikako1992@outlook.com>
2022-01-30 22:02:47 +0900

    remove

--- express/routes/user.js
+++ express/routes/user.js
@@ -12,44 +12,10 @@ const mongodb = require("mongodb")
 // const dbo = require("../db/conn");
 
 // This help convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
 
 // This section will help you create a new record.
 recordRoutes.route("/user/add").post(async function (req, response) {
-  const client = await mongodb.connect(Db, { useUnifiedTopology: true })
-  const db_connect = client.db("meta3oys")
-
-  // let db_connect = dbo.getDb();
-  console.log(db_connect)
-  let myobj = {
-    email: req.body.email,
-    wallet: req.body.wallet,
-    balance: req.body.balance,
-    phone: req.body.phone,
-  };
-  console.log(myobj)
-  db_connect.collection("users").findOne({email: myobj["email"]}, (err, res) => {
-    if (err) throw err;
-    if (res !== null){
-      return response.status(400).send({message: 0})
-    }
-    db_connect.collection("users").findOne({wallet: myobj["wallet"]}, (err, res) => {
-      if (err) throw err;
-      if (res !== null){
-        return response.status(400).send({message: 1})
-      }
-      db_connect.collection("users").findOne({wallet: myobj["phone"]}, (err, res) => {
-        if (err) throw err;
-        if (res !== null){
-          return response.status(400).send({message: 2})
-        }
-        db_connect.collection("users").insertOne(myobj, function (err, res) {
-          if (err) throw err;
-          response.json(res);
-        });
-      }) 
-    }) 
-  })
+  
 });
 
 module.exports = recordRoutes;
